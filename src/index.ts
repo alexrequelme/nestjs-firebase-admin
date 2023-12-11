58d62ea28cd6ff87d0ed873614ac59a3d3333ddc
AlexRequelme <alexdeveloper09@gmail.com>
2023-12-11 14:51:26 -0500

    Add new guard #2

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './firebase-admin.module';
 export * from './decorators';
 export * from './firebase-auth.guard';
+export * from './firebase-public-access.guard';
 export * from './interfaces';
